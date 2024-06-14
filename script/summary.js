3fca816b4a7c4f90eb3fc80be319242028c30fc1
Mike Schauber <mike.schauber96@gmail.com>
2024-06-14 13:36:06 +0200

    hover effects and positioning summary

---
+++ script/summary.js
@@ -0,0 +1,29 @@
+function sumStart() {
+    document.getElementById("firstRowCont1").classList.add("bg-darkblue");
+    document.getElementById("firstRowCont2").classList.add("bg-darkblue");
+};
+
+function changeColor1() {
+    document.getElementById("firstRowCont1").classList.remove("bg-darkblue");
+    document.getElementById('firstRowCont1').classList.add("bg-white");
+    document.getElementById("sumEdit").src = "../assets/icons/edit-black.svg";
+};
+
+function recreateColor1() {
+    document.getElementById('firstRowCont1').classList.remove("bg-white");
+    document.getElementById("firstRowCont1").classList.add("bg-darkblue");
+    document.getElementById("sumEdit").src = "../assets/icons/edit-white.svg";
+};
+
+function changeColor2() {
+    document.getElementById("firstRowCont2").classList.remove("bg-darkblue");
+    document.getElementById('firstRowCont2').classList.add("bg-white");
+    document.getElementById("sumHook").src = "../assets/icons/hook-black.svg";
+}
+
+function recreateColor2() {
+    document.getElementById("firstRowCont2").classList.add("bg-darkblue");
+    document.getElementById('firstRowCont2').classList.remove("bg-white");
+    document.getElementById("sumHook").src = "../assets/icons/hook-white.svg";
+}
+
