a166f8ab724791d10e4484600c26f02a67ce9e78
Jan Steiner <jansteiner73@gmail.com>
2024-06-15 21:03:37 +0200

    register-overlay

--- script/register.js
+++ script/register.js
@@ -9,10 +9,27 @@ let users = [
     }
 ];
 
+function register(event) {
+    event.preventDefault();
+    showOverlay();
+    addUser();
+}
+
 function addUser() {
     let email = document.getElementById("signup-input-email");
     let password = document.getElementById("signup-input-password");
     users.push({ email: email.value, password: password.value })
     //weiterleitung zu login seite
     window.location.href = "login.html?msg=Du hast dich erfolgreich registriert";
+}
+
+function showOverlay() {
+    document.getElementById('register-success-overlay').style.display = 'block';
+    let successMessage = document.getElementById('register-success-message');
+    successMessage.classList.add('show');
+
+    setTimeout(function() {
+        successMessage.classList.remove('show');
+        document.getElementById('register-success-overlay').style.display = 'none';
+    }, 1000);
 }
