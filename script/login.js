7d53f5852dc007f3bc9be0ed03e448dc8e6359a7
felix2302 <felix.kirmse@gmail.com>
2024-06-16 19:44:41 +0200

    fixes

--- script/login.js
+++ script/login.js
@@ -7,7 +7,7 @@ async function login() {
 
     let email = document.getElementById("login-input-email");
     let password = document.getElementById("login-input-password");
-    let user = users.find(u => u.email === email.value && u.password === password.value);
+    user = users.find(u => u.email === email.value && u.password === password.value);
     console.log(user);
     if (user) {
         alert("user gefunden! Wenn alles fertig ist wirst du zum board weitergeleitet :)");
