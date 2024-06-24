4f3ac1ff5a0ac26b72129828f249d8b5f815a40f
Mike Schauber <mike.schauber96@gmail.com>
2024-06-24 19:27:58 +0200

    Add dymanic stats in summary (not finished)

--- script/summary.js
+++ script/summary.js
@@ -1,5 +1,8 @@
+let statusArray = [];
+
 async function sumStart() {
     await includeHTML();
+    await filterTaskStatus()
     document.getElementById("firstRowCont1").classList.add("bg-darkblue");
     document.getElementById("firstRowCont2").classList.add("bg-darkblue");
 };
@@ -27,3 +30,106 @@ function recreateColor2() {
     document.getElementById('firstRowCont2').classList.remove("bg-white");
     document.getElementById("sumHook").src = "../assets/icons/hook-sum-white.png";
 };
+
+async function filterTaskStatus() {
+    let tasks = await loadData(TASKS_URL);
+    let todos = tasks.filter(task => task.status === "todo");
+    let inProgress = tasks.filter(task => task.status === "inprogress");
+    let awaitFeedback = tasks.filter(task => task.status === "awaitfeedback");
+    let done = tasks.filter(task => task.status === "done");
+    let urgent = tasks.filter(task => task.priority === "high");
+    statusArray.push({ "category": todos, "length": todos.length });
+    statusArray.push({ "category": inProgress, "length": inProgress.length });
+    statusArray.push({ "category": awaitFeedback, "length": awaitFeedback.length });
+    statusArray.push({ "category": done, "length": done.length });
+    statusArray.push({ "category": urgent, "length": urgent.length, "due_date": urgent[0]["due_date"]});
+    console.log(tasks);
+    console.log(statusArray);
+    document.getElementById("sumBody").innerHTML = "";
+    document.getElementById("sumBody").innerHTML = returnSummaryHTML(tasks);
+};
+
+function returnSummaryHTML(tasks) {
+    return `
+        <div class="flex-center summary-headline mgn-l-328">
+            <h1>Join 360</h1>
+            <img class="sum-headbar" src="../assets/img/bar-blue.png" alt="bar">
+            <h3>Key Metrics at a Glance</h3>
+        </div>
+        <div class="flex-center summary-headline2 mgn-l-328">
+            <h1>Join 360</h1>
+            <h3>Key Metrics at a Glance</h3>
+            <div class="sum-bluebar-cont">
+                <img src="../assets/img/bar-blue-horizontal.png" alt="bar">
+            </div>
+        </div>
+        <div class="flex-row sum-stats-cont mgn-l-328">
+            <div class="summary-stats flex-column">
+                <div class="flex-row sum-first-row">
+                    <a href="./board.html" onmouseenter="changeColor1()" onmouseleave="recreateColor1()"
+                        class="sum-first-row-cont flex-center flex-row">
+                        <div id="firstRowCont1" class="flex-center sum-icon-container">
+                            <img id="sumEdit" src="../assets/icons/edit-white.svg" alt="">
+                        </div>
+                        <div class="sum-number-container flex-center flex-column">
+                            <div class="sum-number">${statusArray[0].length}</div>
+                            <p>To-do</p>
+                        </div>
+                    </a>
+                    <a href="./board.html" onmouseover="changeColor2()" onmouseleave="recreateColor2()"
+                        class="sum-first-row-cont flex-center flex-row">
+                        <div id="firstRowCont2" class="flex-center sum-icon-container">
+                            <img id="sumHook" src="../assets/icons/hook-sum-white.png" alt="">
+                        </div>
+                        <div class="sum-number-container flex-center flex-column">
+                            <div class="sum-number">${statusArray[3].length}</div>
+                            <p>Done</p>
+                        </div>
+                    </a>
+                </div>
+                <a href="./board.html">
+                    <div class="sum-sec-row flex-row flex-center">
+                        <div class="sum-urgency">
+                            <div class="flex-center sum-icon-container bg-red">
+                                <img class="" src="../assets/icons-addtask/prio-high-white.png" alt="">
+                            </div>
+                            <div class="flex-center flex-column">
+                                <div class="sum-number">${statusArray[4].length || 0}</div>
+                                <p>Urgent</p>
+                            </div>
+                        </div>
+                        <img class="sum-grey-bar" src="../assets/img/bar-grey.png" alt="">
+                        <div class="sum-date flex-center flex-column">
+                            <p><b>${statusArray[4].due_date}</b></p>
+                            <p class="sum-dateline">Upcoming Deadline</p>
+                        </div>
+                    </div>
+                </a>
+                <div class="sum-third-row flex-row flex-center">
+                    <a class="sum-third-cont flex-center bg-white" href="./board.html">
+                        <div class="sum-third-parent flex-center flex-column">
+                            <div class="sum-big-number">${tasks.length}</div>
+                            <p class="text-center">Tasks in Board</p>
+                        </div>
+                    </a>
+                    <a class="sum-third-cont flex-center bg-white" href="./board.html">
+                        <div class="sum-third-parent flex-center flex-column">
+                            <div class="sum-big-number">${statusArray[1].length}</div>
+                            <p class="text-center">Tasks in Progress</p>
+                        </div>
+                    </a>
+                    <a class="sum-third-cont flex-center bg-white" href="./board.html">
+                        <div class="sum-third-parent flex-center flex-column">
+                            <div class="sum-big-number">${statusArray[2].length}</div>
+                            <p class="text-center">Awaiting Feedback</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="sum-greet-cont flex-center flex-column">
+                <h2 class="color-darkblue">Good morning,</h2>
+                <h1 class="color-lightblue">Sofia Müller</h1>
+            </div>
+        </div>
+    `;
+}
