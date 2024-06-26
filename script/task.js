30d97bd455c66f4c86a93506f3063a79670f1068
Mike Schauber <mike.schauber96@gmail.com>
2024-06-26 14:51:16 +0200

    little changes when edit a task

--- script/task.js
+++ script/task.js
@@ -151,17 +151,23 @@ function deleteTask(index) {
 
 function renderEdit(id) {
     taskContainer.innerHTML = `
-    <form class="task-edit-form" onsubmit="addNewTask(); return false;">
-            <div class="flex-column task-content">
-                
+    <div class="task-h1 flex-row task-board-h1">
+            <h1></h1>
+            <img onclick="closeNewTaskInBoard()" src="../assets/icons/x-black.png" alt="x">
+        </div>
+        <form class="form-edit>
+            <div class="flex-row task-content in-edit-task-cnt">
+                <div class="task-left-cont">
                     <div class="task-width">
                         <h3 class="task-form-font">Title<span class="task-star">*</span></h3>
-                        <input id="taskTitle" class="task-width task-form-font task-input" type="text" placeholder="Enter a title" required="">
+                        <input id="taskTitle" class="task-width task-form-font task-input" type="text" placeholder="Enter a title"
+                            required>
                             <span id="requiredTitle" class="required-text" style="display: none;">This field is required</span>
                     </div>
                     <div>
                         <h3 class="task-form-font">Description</h3>
-                        <textarea id="taskDescription" class="task-width task-form-font task-textarea task-input" type="text" placeholder="Enter a Description"></textarea>
+                        <textarea id="taskDescription" class="task-width task-form-font task-textarea task-input" type="text"
+                            placeholder="Enter a Description"></textarea>
                     </div>
                     <div>
                         <h3 class="task-form-font">Assigned to</h3>
@@ -180,33 +186,39 @@ function renderEdit(id) {
                                 </div>
                             </div>
                             <div id="signParentContainer" class="flex-center" style="width: 100%;">
-                                <div id="signContainer" class="task-sign-cont task-width flex-center" style="display: none;">
+                                <div id="signContainer" class="task-sign-cont task-width flex-center"
+                                    style="display: none;">
                                     <!-- rendered signs -->
                                 </div>
                             </div>
                         </div>
                     </div>
-                
-                
+                </div>
+                <div class="task-right-cont task-right-cont-edit" id="taskRightCont">
                     <div class="task-width">
                         <h3 class="task-form-font">Due date<span class="task-star">*</span></h3>
                         <div class="task-date-input task-width">
-                            <input onclick="openCalender()" id="taskDate" class="task-date-input task-form-font color-lightgrey bg-white task-input" type="date" required="">
+                            <input onclick="openCalender()" id="taskDate"
+                                class="task-date-input task-form-font color-lightgrey bg-white task-input" type="date"
+                                required>
                                 <span id="requiredDate" class="required-text" style="display: none;">This field is required</span>
                         </div>
                     </div>
                     <div>
                         <h3 class="task-form-font">Prio</h3>
                         <div class="flex-row task-btn-cont">
-                            <div onclick="changeUrgency('high')" onmouseover="hoverBtn(true, 'img-high')" onmouseout="hoverBtn(false, 'img-high')" class="task-urgent-btn bg-white" id="high">
+                            <div onclick="changeUrgency('high')" onmouseover="hoverBtn(true, 'img-high')"
+                                onmouseout="hoverBtn(false, 'img-high')" class="task-urgent-btn bg-white" id="high">
                                 <span>Urgent</span>
                                 <img id="img-high" src="../assets/icons-addtask/prio-high-color.png" alt="">
                             </div>
-                            <div onclick="changeUrgency('mid')" onmouseover="hoverBtn(true, 'img-mid')" onmouseout="hoverBtn(false, 'img-mid')" class="task-urgent-btn bg-white mid-focus" id="mid">
+                            <div onclick="changeUrgency('mid')" onmouseover="hoverBtn(true, 'img-mid')"
+                                onmouseout="hoverBtn(false, 'img-mid')" class="task-urgent-btn bg-white" id="mid">
                                 <span>Medium</span>
                                 <img id="img-mid" src="../assets/icons-addtask/prio-mid-white.png" alt="">
                             </div>
-                            <div onclick="changeUrgency('low')" onmouseover="hoverBtn(true, 'img-low')" onmouseout="hoverBtn(false, 'img-low')" class="task-urgent-btn bg-white" id="low">
+                            <div onclick="changeUrgency('low')" onmouseover="hoverBtn(true, 'img-low')"
+                                onmouseout="hoverBtn(false, 'img-low')" class="task-urgent-btn bg-white" id="low">
                                 <span>Low</span>
                                 <img id="img-low" src="../assets/icons-addtask/prio-low-color.png" alt="">
                             </div>
@@ -216,7 +228,8 @@ function renderEdit(id) {
                         <h3 class="task-form-font">Category<span class="task-star">*</span></h3>
                         <div onclick="openCategorys(); stopProp(event);" class="dropdown task-width">
                             <div class="dropdown-toggle ctg-input-cnt" id="dropdownCategoryToggle">
-                                <input onclick="openCategorys(); preventDf(event); stopProp(event);" id="categoryInput" type="text" placeholder="Select task category" class="category-dd-upper-item-input task-width bg-white" required="">
+                                <input onclick="openCategorys(); preventDf(event); stopProp(event);" id="categoryInput" type="text" placeholder="Select task category" 
+                                class="category-dd-upper-item-input task-width bg-white" required>
                                 <img src="../assets/icons/dropdown.png">
                             </div>
                             <div class="dropdownmenu-ctg" id="dropdownCategoryContainer" style="display: none;">
@@ -233,9 +246,10 @@ function renderEdit(id) {
                     </div>
                     <div>
                         <h3 class="task-form-font">Subtasks</h3>
-                        <div>
+                        <div >
                             <div onkeydown="checkKey(event)" onclick="openSubtasks(); stopProp(event);" id="subtaskInputCont" class="task-width task-form-font subtask-input-cont">
-                                <input id="subtasksInput" class="subtasks-input task-input" type="text" placeholder="Add new subtask">
+                                <input id="subtasksInput" class="subtasks-input task-input" type="text"
+                                    placeholder="Add new subtask">
                                 <div id="subtaskImgCont" class="subtask-img-cont flex-center">
                                     <div class="img-cont-subtask">
                                         <img src="../assets/icons/add.png" alt="">
@@ -248,17 +262,17 @@ function renderEdit(id) {
                             </div>
                         </div>
                     </div>
-                
+                </div>
             </div>
-            <div class="task-bottom-line-cont flex-center">
+            <div class="task-bottom-line-cont add-task-bottom-line-board flex-center">
                 <div class="flex-row flex-center task-bottom-line">
-                  
+                    <p><span class="task-star">*</span>This field is required</p>
                     <div class="flex-center task-form-btn-cont">
                         <div class="task-clear-btn" onclick="clearAll()" onmouseover="enterIcon()" onmouseout="outIcon()">
                             <span>Clear</span>
                             <img id="task-x" src="../assets/icons/x-black.png" alt="">
                         </div>
-                        <button onclick="formValidationFeedbackOn()" type="submit" id="createButton" class="task-send-form-btn">
+                        <button onclick="addNewTask(); return false" type="submit" id="createButton" class="task-send-form-btn">
                             <span>Create Task</span>
                             <img src="../assets/icons/hook-white.svg" alt="">
                         </button>
