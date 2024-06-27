ae06113c096f653196f114696b9a9107bcd691b4
MikeSchauber <mike.schauber96@gmail.com>
2024-06-27 11:23:44 +0200

    save random number in script.js

--- script/script.js
+++ script/script.js
@@ -131,6 +131,31 @@ function loadUser() {
     }
 };
 
+let backgroundColors = [
+    "rgba(255, 122, 0, 1)",
+    "rgba(255, 94, 179, 1)",
+    "rgba(110, 82, 255, 1)",
+    "rgba(147, 39, 255, 1)",
+    "rgba(0, 190, 232, 1)",
+    "rgba(31, 215, 193, 1)",
+    "rgba(255, 116, 94, 1)",
+    "rgba(255, 163, 94, 1)",
+    "rgba(252, 113, 255, 1)",
+    "rgba(255, 199, 1, 1)",
+    "rgba(0, 56, 255, 1)",
+    "rgba(195, 255, 43, 1)",
+    "rgba(255, 230, 43, 1)",
+    "rgba(255, 70, 70, 1)",
+    "rgba(255, 187, 43, 1)"
+];
+
+function setRandomNumber() {
+    let number = Math.random();
+    number = number.toFixed(2) * 15;
+    number = Math.floor(number);
+    return number;
+};
+
 if (window.location.pathname.includes('/html/') && !window.location.pathname.includes('register')) {
     loadUser();
 };
