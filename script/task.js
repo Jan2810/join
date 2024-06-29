1583b50d1a1bcf3341c0413204fa012c43f119d9
Felix <felix.kirmse@gmail.com>
2024-06-29 15:01:24 +0200

    show task changes

--- script/task.js
+++ script/task.js
@@ -1,47 +1,43 @@
 const body = document.querySelector("body");
 let bg = document.createElement("div");
-let taskContainer = document.createElement("div")
-
+let taskContainer = document.createElement("div");
+taskContainer.classList.add("task-container", "flex-column", "task-container-hidden");
 
 function prepareTask() {
-    bg.classList.add("task-bg", "task-bg-low-index", "flex-center");
-    body.appendChild(bg);
-    taskContainer.classList.add("task-container", "flex-column", "task-container-hidden");
+    bg.classList.add("d-none");
+    bg.classList.remove("bg-active",);
+    body.insertAdjacentElement("beforeend", bg);
+    body.classList.remove("overflow-hidden");
     bg.appendChild(taskContainer);
-
 }
 
 function showTask(id) {
-    taskContainer.classList.remove("d-none");
+    bg.classList.remove("d-none");
+    bg.classList.add("bg-active");
+    setTimeout(() => {
+        taskContainer.classList.remove("task-container-hidden");
+    }, 0);
     taskContainer.innerHTML = renderTask(id);
-    taskContainer.classList.remove("task-container-hidden");
-    taskContainer.classList.add("flex-column");
-    bg.classList.toggle("task-bg-active");
-    bg.classList.toggle("task-bg-low-index");
-
-    body.classList.toggle("overflow-hidden");
-
-
+    body.classList.add("overflow-hidden");
     bg.addEventListener('click', function (event) {
         if (event.target == this) {
             closeTask();
         }
     });
-
 }
 
 
 function closeTask() {
     taskContainer.classList.add("task-container-hidden");
-    bg.classList.remove("task-bg-active");
-    body.classList.remove("overflow-hidden");
-    bg.classList.add("task-bg-low-index");
+    setTimeout(() => {
+        prepareTask();
+    }, 100);
 }
 
-function getActualTask(id) {
+function getTaskIndex(id) {
     let index = tasksArray.findIndex((e) => e['id'] == id);
-    let task = tasksArray[index];
-    return { element: task, index: index };
+    // let task = tasksArray[index];
+    return index;
 }
 
 
@@ -91,9 +87,9 @@ function renderTodos(task) {
     else { return "" }
 };
 
-function strike(index) {
-    let todoInDom = document.getElementById(`todo${index}`);
-    let todoValue = document.getElementById(`todovalue${index}`);
+function strikeThroughTodo(indexOfTodo) {
+    let todoInDom = document.getElementById(`todo${indexOfTodo}`);
+    let todoValue = document.getElementById(`todovalue${indexOfTodo}`);
     if (todoInDom.checked) {
 
         todoValue.classList.add("line-through");
@@ -103,11 +99,11 @@ function strike(index) {
     }
 }
 
-function updateTodoStatus(id, index) {
-    taskIndex = tasksArray.findIndex(element => element.id === id);
+function updateTodoStatus(id, indexOfTodo) {
+    let taskIndex = (getTaskIndex(id));
     let task = tasksArray[taskIndex];
-    let todo = task.subtasks[index];
-    let todoInDom = document.getElementById(`todo${index}`);
+    let todo = task.subtasks[indexOfTodo];
+    let todoInDom = document.getElementById(`todo${indexOfTodo}`);
 
     todoInDom.addEventListener("change", () => {
         if (todo.status == "checked") {
@@ -123,40 +119,26 @@ function updateTodoStatus(id, index) {
 
     }
     )
-    strike(index);
+    strikeThroughTodo(indexOfTodo);
 }
 
-
-
-
-
-
-
-function findIndex(id) {
-    let index = tasksArray.findIndex(element => element.id === id);
-    deleteTask(index);
-};
 
 
 function deleteTask(index) {
     tasksArray.splice(index, 1);
-
-    bg.classList.toggle("task-bg-active");
-    bg.classList.toggle("task-bg-low-index");
-    taskContainer.classList.remove("flex-column");
-    taskContainer.classList.toggle("d-none");
+    putData(TASKS_URL, tasksArray)
+    prepareTask();
     initBoard()
 }
 
 
-function renderEdit(id) {
+function renderEdit(taskIndex) {
     console.clear();
     console.log("Alle Tasks:");
     console.log(tasksArray);
-    let task = (getActualTask(id)).element;
-    let taskId = (getActualTask(id)).index;
+    let task = tasksArray[taskIndex];
     console.log("Aktuelle TaskID:");
-    console.log(taskId);
+    console.log(taskIndex);
     console.log("Aktueller Task als Objekt:");
     console.log(task);
 
@@ -279,7 +261,7 @@ function renderEdit(id) {
             <div class="task-bottom-line-cont flex-center">
                 <div class="flex-row flex-center task-bottom-line bottom-line-edit">
                     <div class="flex-center task-form-btn-cont">
-                        <button onclick="addNewTask(); return false" type="submit" id="createButtonEdit" class="task-send-form-btn">
+                        <button onclick="saveTask(); return false" type="submit" id="createButtonEdit" class="task-send-form-btn">
                             <span>Ok</span>
                             <img src="../assets/icons/hook-white.svg" alt="">
                         </button>
@@ -296,10 +278,10 @@ function renderTask(id) {
     console.clear();
     console.log("Alle Tasks:");
     console.log(tasksArray);
-    let task = (getActualTask(id)).element;
-    let taskId = (getActualTask(id)).index;
+    let taskIndex = (getTaskIndex(id));
+    let task = tasksArray[taskIndex];
     console.log("Aktuelle TaskID:");
-    console.log(taskId);
+    console.log(taskIndex);
     console.log("Aktueller Task als Objekt:");
     console.log(task);
 
@@ -330,10 +312,10 @@ function renderTask(id) {
                             d="M3 18C2.45 18 1.97917 17.8042 1.5875 17.4125C1.19583 17.0208 1 16.55 1 16V3C0.716667 3 0.479167 2.90417 0.2875 2.7125C0.0958333 2.52083 0 2.28333 0 2C0 1.71667 0.0958333 1.47917 0.2875 1.2875C0.479167 1.09583 0.716667 1 1 1H5C5 0.716667 5.09583 0.479167 5.2875 0.2875C5.47917 0.0958333 5.71667 0 6 0H10C10.2833 0 10.5208 0.0958333 10.7125 0.2875C10.9042 0.479167 11 0.716667 11 1H15C15.2833 1 15.5208 1.09583 15.7125 1.2875C15.9042 1.47917 16 1.71667 16 2C16 2.28333 15.9042 2.52083 15.7125 2.7125C15.5208 2.90417 15.2833 3 15 3V16C15 16.55 14.8042 17.0208 14.4125 17.4125C14.0208 17.8042 13.55 18 13 18H3ZM3 3V16H13V3H3ZM5 13C5 13.2833 5.09583 13.5208 5.2875 13.7125C5.47917 13.9042 5.71667 14 6 14C6.28333 14 6.52083 13.9042 6.7125 13.7125C6.90417 13.5208 7 13.2833 7 13V6C7 5.71667 6.90417 5.47917 6.7125 5.2875C6.52083 5.09583 6.28333 5 6 5C5.71667 5 5.47917 5.09583 5.2875 5.2875C5.09583 5.47917 5 5.71667 5 6V13ZM9 13C9 13.2833 9.09583 13.5208 9.2875 13.7125C9.47917 13.9042 9.71667 14 10 14C10.2833 14 10.5208 13.9042 10.7125 13.7125C10.9042 13.5208 11 13.2833 11 13V6C11 5.71667 10.9042 5.47917 10.7125 5.2875C10.5208 5.09583 10.2833 5 10 5C9.71667 5 9.47917 5.09583 9.2875 5.2875C9.09583 5.47917 9 5.71667 9 6V13Z"
                             fill="#2A3647" />
                     </svg>
-                    <span onclick="findIndex('${id}')">Delete</span>
+                    <span onclick="deleteTask('${taskIndex}')">Delete</span>
                 </div>
                 <div class="task-footer-divider"></div>
-                <div onclick="renderEdit('${id}')" class="task-edit-container flex">
+                <div onclick="renderEdit('${taskIndex}')" class="task-edit-container flex">
                     <svg width="18" height="18" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M3.16667 22.3332H5.03333L16.5333 10.8332L14.6667 8.9665L3.16667 20.4665V22.3332ZM22.2333 8.89984L16.5667 3.29984L18.4333 1.43317C18.9444 0.922059 19.5722 0.666504 20.3167 0.666504C21.0611 0.666504 21.6889 0.922059 22.2 1.43317L24.0667 3.29984C24.5778 3.81095 24.8444 4.42761 24.8667 5.14984C24.8889 5.87206 24.6444 6.48873 24.1333 6.99984L22.2333 8.89984ZM20.3 10.8665L6.16667 24.9998H0.5V19.3332L14.6333 5.19984L20.3 10.8665Z"
