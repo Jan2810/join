22277d6a9d8fbc0a8f2b1255e56ed523a89d5863
MikeSchauber <mike.schauber96@gmail.com>
2024-07-01 13:59:48 +0200

    delete comments in taskEdit.js

--- script/taskEdit.js
+++ script/taskEdit.js
@@ -31,7 +31,6 @@ async function putEditTask() {
 
     initBoard();
     taskContainer.innerHTML = renderTask(taskDataEdit.id);
-    // closeTask();
 };
 
 async function initDataEditTask(task, id) {
