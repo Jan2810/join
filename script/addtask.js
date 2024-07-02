459866b467977e11ae4dfb4c80370c6b6cd9cf66
Mike Schauber <mike.schauber96@gmail.com>
2024-07-02 23:24:20 +0200

    Revert "fixed wrong if else in addtask"
    This reverts commit c19b7481fd90a33fdebebe452716b5e39f494fd0.

--- script/addtask.js
+++ script/addtask.js
@@ -281,7 +281,9 @@ async function getAssignedContacts(contacts) {
         for (let i = 0; i < checkedContacts.length; i++) {
             const assignedContact = checkedContacts[i];
             if (assignedContact == true) {
-                taskData.assigned_to.push(contacts[i]);
+                if (taskData.assigned_to.length <= 11) {
+                    taskData.assigned_to.push(contacts[i]);
+                }
             }
         }
     } else {
