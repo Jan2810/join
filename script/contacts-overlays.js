31f73cd438fef063210288cecf38babb28d7c039
Felix2302 <felix.kirmse@gmail.com>
2024-07-06 17:36:43 +0200

    overlay contacts

--- script/contacts-overlays.js
+++ script/contacts-overlays.js
@@ -181,7 +181,7 @@ function doNotClose(event) {
  * Hides the edit/delete popup on small screens.
  */
 function hideEditDeletePopup() {
-    if (window.innerWidth < 800) {
+    if (window.innerWidth < 1200) {
         editDeletePopup.classList.add('hide-edit-delete-popup');
         setTimeout(function () {
             singleContactTripleDots.classList.remove('d-none');
